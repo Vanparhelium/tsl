724eaf395022fc18961089d02063a47effda0a02
Vanparhelium <Vanparhelium@gmail.com>
2021-05-01 15:22:53 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+// menu
+
 window.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#burger').addEventListener('click', function () {
     document.querySelector('#menu').classList.toggle('header__list_visible');
     document.querySelector('#burger').classList.toggle('menu__btn');
     document.querySelector('#burger').classList.toggle('menu__btn-close');
     });
+
+
+
 });
+
